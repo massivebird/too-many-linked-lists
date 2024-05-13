aca66a964b7e904dab01219297f417d734bbc3c2
massivebird <gdrakemail@gmail.com>
2024-05-12 20:00:26 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+// A linked list can be defined as being in one of two states:
+// + Empty, or
+// + Consisting of a node followed by another list!
+//
+// At first, we might build a Node enum. But if we make an enum of variants
+// Nil and Cons(T, Box<List<T>>), we're building entire nodes dedicated to demarcating the
+// end of a list. Plus, there are some implications of nodes being on the stack AND the heap.
+// This is no good! Ideally:
+// + All nodes are allocated in the same place (uniform allocation)
+// + The list tail does not allocate excessive memory (see: null ptr optimization)
+//
+// We can achieve this by representing _nodes_ and _node relationships_ separately!
+// Also, creating a pub struct that wraps these two allows us to keep the other two
+// private.
+
+// struct w single field -> zero cost abstraction!
+#[derive(Debug)]
+pub struct List<T> {
+    head: Link<T>,
+}
+
+#[derive(Debug)]
+struct Node<T> {
+    value: T,
+    next: Link<T>,
+}
+
+// Represents the relationship between nodes.
+// Takes advantage of null pointer optimization by having two variants: one with no associated
+// data, and the other with one associated pointer.
+#[derive(Debug)]
+enum Link<T> {
+    Nil,
+    // Self cannot contain Self — the recursion makes it impossible for Rust to calculate
+    // the memory allocation requirements of Self.
+    // We require a layer of indirection, which is possible via `Box`!
+    Cons(Box<Node<T>>),
+}
+
+fn main() {}
