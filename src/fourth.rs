941083725023b0ee497ce08c85c5aa618e0f4c5f
massivebird <gdrakemail@gmail.com>
2024-06-27 18:50:17 -0400

    added: constructors

--- src/fourth.rs
+++ src/fourth.rs
@@ -13,3 +13,22 @@ struct Node<T> {
     next: Link<T>,
     prev: Link<T>,
 }
+
+impl<T> List<T> {
+    const fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+        }
+    }
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
+            elem,
+            next: None,
+            prev: None,
+        }))
+    }
+}
