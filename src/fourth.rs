52ce32400b379ded2c94cc520d515883188542fb
massivebird <gdrakemail@gmail.com>
2024-06-27 19:12:32 -0400

    added: pop_front

--- src/fourth.rs
+++ src/fourth.rs
@@ -36,6 +36,22 @@ impl<T> List<T> {
             }
         }
     }
+
+    fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    new_head.borrow_mut().prev.take();
+                    self.head = Some(new_head);
+                }
+                None => {
+                    // list is emptied after this pop
+                    self.tail.take();
+                }
+            }
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    }
 }
 
 impl<T> Node<T> {
