580240dc2960e973761edbe576dea41c943261a7
massivebird <gdrakemail@gmail.com>
2024-06-27 18:59:49 -0400

    added: push_front

--- src/fourth.rs
+++ src/fourth.rs
@@ -21,6 +21,21 @@ impl<T> List<T> {
             tail: None,
         }
     }
+
+    fn push_front(&mut self, elem: T) {
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                self.head = Some(new_head.clone());
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+            }
+            None => {
+                self.head = Some(new_head.clone());
+                self.tail = Some(new_head);
+            }
+        }
+    }
 }
 
 impl<T> Node<T> {
