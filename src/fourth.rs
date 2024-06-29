30aeccca0725f0ea6de3f7e8ddea157bcac2c813
massivebird <gdrakemail@gmail.com>
2024-06-29 15:25:22 -0400

    added: push_back and pop_back tests

--- src/fourth.rs
+++ src/fourth.rs
@@ -157,6 +157,32 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+
+        // ---- back -----
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_back(), None);
+
+        // Populate list
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_back(4);
+        list.push_back(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(5));
+        assert_eq!(list.pop_back(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
     }
 
     #[test]
@@ -169,5 +195,6 @@ mod test {
         list.push_front(2);
 
         assert_eq!(*list.peek_front().unwrap(), 2);
+        assert_eq!(*list.peek_back().unwrap(), 5);
     }
 }
