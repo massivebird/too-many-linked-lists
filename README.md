3a21e3eed72b9ef4deadbcb2481cea4c9b2c48cf
massivebird <gdrakemail@gmail.com>
2024-07-09 17:33:22 -0400

    changed: project name to too-many-linked-lists

---
+++ README.md
@@ -0,0 +1,3 @@
+# Too Many Linked Lists
+
+I'm following the [Learning Rust With Entirely Too Many Linked Lists](https://rust-unofficial.github.io/too-many-lists/index.html) book and not understanding
--- src/fifth.rs
+++ src/fifth.rs
@@ -75,6 +75,14 @@ impl<T> List<T> {
             }
         }
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        unsafe { self.head.as_ref().map(|node| &node.elem) }
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        unsafe { self.head.as_mut().map(|node| &mut node.elem) }
+    }
 }
 
 impl<T> Drop for List<T> {
